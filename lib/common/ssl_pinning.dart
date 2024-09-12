7921f745a5bfbc58334740e0054bcf6cec22e501
ats <anangtsap@gmail.com>
2024-09-12 10:36:59 +0700

    refractoring ssl pinning

--- lib/common/ssl_pinning.dart
+++ lib/common/ssl_pinning.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/services.dart';
 
-class SSLPinningContext {
+class SSLPinning {
   Future<SecurityContext> get sslContext async {
     final sslCert = await rootBundle.load('assets/certs/themoviedb-org.pem');
     SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
